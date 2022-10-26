26eae93f8cd52523e8e9cb46590e089e8e810f7b
Ernesto_Gimeno <ergigar@alumni.uv.es>
2022-10-26 12:13:05 +0200

    Creación clase medicamento(sin acabar)

---
+++ ISII-JavaProject/src/main/java/Modelo/Medicamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author ernes
+ */
+public class Medicamento {
+    private String nombre;
+    private String enfermedad_cura;
+    private String alergia;
+    private String efecto_s;
+}

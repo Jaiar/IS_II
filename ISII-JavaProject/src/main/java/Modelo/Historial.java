276a51fd4f22d8198d2c4e2e7a943df284eeea61
Mehdi Malik Ould Abderrahmane <mehould@alumni.uv.es>
2022-10-26 12:56:24 +0200

    Historial modificado

--- ISII-JavaProject/src/main/java/Modelo/Historial.java
+++ ISII-JavaProject/src/main/java/Modelo/Historial.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo;
-
+import java.util.ArrayList;
 /**
  *
  * @author Mehdi
@@ -12,6 +12,41 @@ public class Historial {
     private String DNI;
     private String fecha;
     private String enfermedad;
+   private ArrayList<String> fechas = new ArrayList<String>();
+   private ArrayList<String> enfermedades = new ArrayList<String>();
    
+   public void setDNI(String newDNI) {
+    this.DNI = newDNI;
+  }
+    public void setfecha(String newfecha) {
+    this.fecha = newfecha;
+  }
+     public void setenfermedad(String newenfermedad) {
+    this.enfermedad = newenfermedad;
+  }
+      public void setfechas(ArrayList newfechas) {
+    this.fechas = newfechas;
+  }
+       public void setenfermedades(ArrayList newenfermedades) {
+    this.enfermedades = newenfermedades;
+  }
+      public String geDNI() {
+    return DNI;
+  }
+       public String getfecha() {
+    return fecha;
+  }
+        public String getenfermedad() {
+    return enfermedad;
+  }
+        public ArrayList<String> getfechas() {
+    return fechas;
+        }
+     public ArrayList<String> getenfermedades() {
+    return enfermedades;
+  }
 }
+  
+
+   
  

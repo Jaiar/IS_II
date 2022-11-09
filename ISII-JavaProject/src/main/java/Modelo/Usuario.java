1df93e2bbed5e4227da0e9b4d0e96cf3e85ee175
Alejandro <alejandro.garcia.dlf@gmail.com>
2022-11-09 11:54:57 +0100

    Creado clase Modelo: Usuario

---
+++ ISII-JavaProject/src/main/java/Modelo/Usuario.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author algar
+ */
+public class Usuario {
+    protected int code;
+    protected String nombre;
+    protected String apellidos;
+    protected String dni;
+    protected String telefono;
+    private Date incorporacion; //Es una fecha
+    
+    public Usuario (int code, String nombre, String apellidos, String dni, String telefono, Date incorporacion){
+        this.code = code;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.incorporacion = incorporacion;
+    }
+}

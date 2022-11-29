effe54a4747c59ab916dca7b40f471f8d2145ed9
rrajo <rrajo@DESKTOP-IH79VP2>
2022-11-29 17:09:15 +0100

    DAOpacientes Creación consulta

---
+++ ISII-JavaProject/src/main/java/Controlador/DAOpacientes.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import Modelo.Paciente;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ *
+ * @author rrajo
+ */
+public class DAOpacientes {
+    
+}

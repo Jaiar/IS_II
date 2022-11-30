95cde929148aac7a0866be8de7cbdad80e1f3499
rrajo <rrajo@DESKTOP-IH79VP2>
2022-11-30 02:03:31 +0100

    DAOpacientes + Data enfermos Creacion del dao pacientes que hace devuelve todos los pacientes que hay en el hospital.
    Modificación del Data enfermeros para rellenar el vector con los datos 
    del DAO

--- ISII-JavaProject/src/main/java/Controlador/DAOpacientes.java
+++ ISII-JavaProject/src/main/java/Controlador/DAOpacientes.java
@@ -5,18 +5,52 @@
 package Controlador;
 
 import Modelo.Paciente;
-
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
-import java.sql.SQLException;
-import java.util.Date;
 
 /**
  *
  * @author rrajo
  */
-public class DAOpacientes {
+public class DAOpacientes extends DAO{
     
+    public static ArrayList getPacientes(){
+        ResultSet resultados = null;
+        ArrayList<Paciente> pacientes = new ArrayList<Paciente>();
+        try {
+         String con;
+         Statement s = DAOmedico.getConnection().createStatement();
+         // Consulta SQL
+         con = "SELECT * FROM paciente";
+         resultados = s.executeQuery(con);
+         }
+        catch (Exception e) { // Error en al realizar la consulta
+            System.out.println("Error en la petición a la BD");
+        }
+        
+        try{
+            ArrayList<String>  enfermedades = new ArrayList<String> ();
+            while(resultados.next()){
+                String dni = resultados.getNString(2);
+                String nombre = resultados.getNString(3);
+                String apellidos = resultados.getNString(4);
+                int habitacion = resultados.getInt(6);
+                int medico_p_id = resultados.getInt(7);
+                int enfermero_id = resultados.getInt(8);
+                while(resultados.next()){
+                    if(resultados.getNString(2) == dni)
+                        enfermedades.add(resultados.getNString(9));
+                }
+                pacientes.add(new Paciente(dni, nombre, apellidos, enfermedades, medico_p_id, enfermero_id, habitacion));
+            }
+        }
+        catch(SQLException sqle){
+            System.out.println("Error en la retirada de datos: " + sqle.getMessage());
+            return null;
+        }
+        
+        return pacientes;
+    }
 }

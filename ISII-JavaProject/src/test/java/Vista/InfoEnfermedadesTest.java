f6c88157d111482959517de772d898872dff82d3
Mehdi Malik Ould Abderrahmane <mehould@alumni.uv.es>
2022-12-13 12:35:56 +0100

    InfoEnfermedadesTest creada

---
+++ ISII-JavaProject/src/test/java/Vista/InfoEnfermedadesTest.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package Vista;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luis
+ */
+public class InfoEnfermedadesTest {
+    
+    public InfoEnfermedadesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        fail("el test falla");
+    }
+    
+    @Before
+    public void setUp() {
+       fail("el test falla");
+    }
+    
+    @After
+    public void tearDown() {
+       fail("el test falla");
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
